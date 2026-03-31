9c8fb75d3a23e705c67791ede76f3febcfa67760
Lexi <alkeller@inprs.in.gov>
2026-03-31 16:01:50 -0400

    adjustments

--- javascript/forms.js
+++ javascript/forms.js
@@ -169,9 +169,7 @@ function render() {
         </div>
         <div class="table-cell col-doc">
           <a class="doc-link" href="${f.url}" target="_blank" rel="noopener">
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/>
-            </svg>
+            <i class="ph ph-file"></i>
             View
           </a>
         </div>
