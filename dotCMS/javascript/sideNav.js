8f84798205168473bbfc936c3cd53703445d1b92
Lexi <alkeller@inprs.in.gov>
2026-04-29 09:47:09 -0400

    dotCMS stuff

--- dotCMS/javascript/sideNav.js
+++ dotCMS/javascript/sideNav.js
@@ -1,11 +1,11 @@
-## sidenav.js
-## Location: vtl/javascript/sidenav.js
+// sidenav.js
+// Location: vtl/javascript/sidenav.js
 
 var navItems = document.querySelectorAll('.nav-item');
 
 function showContent(tabId) {
     // Update active states
-    navItems.forEach(function(item) {
+    navItems.forEach(function (item) {
         item.classList.remove('active');
     });
 
@@ -26,8 +26,8 @@ function showContent(tabId) {
     var contentContainer = document.querySelector('.content-area');
     if (contentContainer) {
         fetch('?tab=' + tabId)
-            .then(function(response) { return response.text(); })
-            .then(function(html) {
+            .then(function (response) { return response.text(); })
+            .then(function (html) {
                 var parser = new DOMParser();
                 var doc = parser.parseFromString(html, 'text/html');
                 var newContent = doc.querySelector('.content-area');
@@ -42,11 +42,11 @@ function closeAllDropdowns() {
     var allSubItems = document.querySelectorAll('.nav-item.sub');
     var allDropdownIcons = document.querySelectorAll('.dropdown-arrow');
 
-    allSubItems.forEach(function(item) {
+    allSubItems.forEach(function (item) {
         item.classList.remove('show');
     });
 
-    allDropdownIcons.forEach(function(icon) {
+    allDropdownIcons.forEach(function (icon) {
         icon.classList.remove('expanded');
     });
 }
@@ -56,13 +56,13 @@ function toggleDropdown(parentTab) {
     var dropdownIcon = document.querySelector('[data-tab="' + parentTab + '"] .dropdown-arrow');
 
     var isExpanded = false;
-    subItems.forEach(function(item) {
+    subItems.forEach(function (item) {
         if (item.classList.contains('show')) {
             isExpanded = true;
         }
     });
 
-    subItems.forEach(function(item) {
+    subItems.forEach(function (item) {
         if (isExpanded) {
             item.classList.remove('show');
         } else {
@@ -80,20 +80,20 @@ function toggleDropdown(parentTab) {
 }
 
 // Add click event listeners
-navItems.forEach(function(item) {
-    item.addEventListener('click', function() {
+navItems.forEach(function (item) {
+    item.addEventListener('click', function () {
         var tabId = this.getAttribute('data-tab');
         var hasDropdown = this.getAttribute('data-has-dropdown');
         var isSubItem = this.classList.contains('sub');
 
         if (hasDropdown) {
             var otherDropdowns = document.querySelectorAll('[data-has-dropdown]');
-            otherDropdowns.forEach(function(dropdown) {
+            otherDropdowns.forEach(function (dropdown) {
                 var dropdownTab = dropdown.getAttribute('data-tab');
                 if (dropdownTab !== tabId) {
                     var subItems = document.querySelectorAll('.nav-item.sub[data-parent="' + dropdownTab + '"]');
                     var icon = document.querySelector('[data-tab="' + dropdownTab + '"] .dropdown-arrow');
-                    subItems.forEach(function(sub) { sub.classList.remove('show'); });
+                    subItems.forEach(function (sub) { sub.classList.remove('show'); });
                     if (icon) { icon.classList.remove('expanded'); }
                 }
             });
